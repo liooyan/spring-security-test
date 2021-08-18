760f17b02c1eba0a9b741e99978aead0612bdebc
liy <1064012668@qq.com>
2021-08-19 01:32:17 +0800

    跑通测试

--- src/main/java/com/security/WebSecurityConfig.java
+++ src/main/java/com/security/WebSecurityConfig.java
@@ -6,11 +6,18 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
+    /**
+     *
+     * {@linkplain HttpServletRequest#login(java.lang.String, java.lang.String))}
+     *
+     */
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
@@ -29,6 +36,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+                .passwordEncoder(new MyPasswordEncoder())
+                .withUser("user").password("password").roles("ADMIN");
+    }
+
+     class MyPasswordEncoder implements PasswordEncoder {
+        @Override
+        public String encode(CharSequence charSequence) {
+            //加密方法可以根据自己的需要修改
+            return charSequence.toString();
+        }
+
+        @Override
+        public boolean matches(CharSequence charSequence, String s) {
+            //加密解密还要写专门的工具类，所以这里没有加密，只是简单的对比
+            return encode(charSequence).equals(s);
+        }
     }
 }
